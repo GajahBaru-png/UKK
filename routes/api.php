a7498c4b7eb33fde1d6ac1627bb94602da5e6bcb
unknown <mbahjarot015@gmail.com>
2025-05-30 22:45:20 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SiswaController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('siswa', SiswaController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
